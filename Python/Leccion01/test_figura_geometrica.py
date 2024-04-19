f25ebe48f9e9224f2c3759d9fd7d46dfb611ad87
GabrielGonzalez11 <135771999+GabrielGonzalez11@users.noreply.github.com>
2024-04-19 15:12:47 -0300

    Agrego codigo de videos 7-9

--- Python/Leccion01/test_figura_geometrica.py
+++ Python/Leccion01/test_figura_geometrica.py
@@ -7,3 +7,8 @@ print(f'Calculo del area del cuadrado: {cuadrado1.calcular_area()}')
 
 # MRO = METHOD RESOLUTION ORDER (muestra el orden en que se ejecutan los metodos)
 print(Cuadrado.mro())
+print(cuadrado1)
+
+rectangulo1 = Rectangulo(3, 8, 'Verde')
+print(f'Calculo del area del rectangulo: {rectangulo1.calcular_area()}')
+print(rectangulo1)
