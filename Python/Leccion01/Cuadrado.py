ec37d2db2b0d2cfc446635b72a058061d9310f2a
Angelo Vellar <angelovellar24@gmail.com>
2024-04-25 14:01:28 -0300

    Update: Corrigiendo Errores

--- Python/Leccion01/Cuadrado.py
+++ Python/Leccion01/Cuadrado.py
@@ -5,11 +5,10 @@ class Cuadrado(FiguraGeometrica, Color):
     def __init__(self, lado, color):
         FiguraGeometrica.__init__(self, lado, lado)
         Color.__init__(self, color)
-        
+
     def calcular_area(self):
         return self._alto * self._ancho
-    
+
     def __str__(self):
         return f'{FiguraGeometrica.__str__(self)} {Color.__str__(self)}'
-    
-    
+
--- Python/Leccion01/FiguraGeometrica.py
+++ Python/Leccion01/FiguraGeometrica.py
@@ -1,21 +1,42 @@
-class FiguraGeometrica:
+from abc import ABC, abstractmethod
+# ABS SIGNIFICA: Abstract Base Class, convierte una clase en abstracta
+class FiguraGeometrica(ABC):
     def __init__(self, ancho, alto):
-        self._ancho = ancho
-        self._alto = alto
-        
+        if self._validar_valores(ancho):
+            self._ancho = ancho
+        else:
+            self.ancho = 0
+            print(f"Valor erroneo para el ancho: {ancho}")
+        if 0 < alto < 10:
+            self._alto = alto
+        else:
+            self.alto = 0
+            print(f"Valor erroneo para el alto: {alto} ")
     @property
     def ancho(self):
         return self._ancho
     @ancho.setter
     def ancho(self, ancho):
-        self._ancho = ancho
+        if self._validar_valores(ancho):
+            self.ancho = ancho
+        else:
+            print(f"Valor erroneo ancho: {ancho}")
     @property
-    def alto(self):
+    def alto(self,alto):
         return self._alto
-    @ancho.setter
+    @alto.setter
     def alto(self, alto):
-        self._alto = alto
-    
+        if self._validar_valores(alto):
+            self._alto = alto
+        else:
+            print(f"Valor erroneo alto: {alto} ")
+
+    @abstractmethod
+    def calcular_area(self):
+        pass
+
     def __str__(self):
         return f'Figura Geometrica [Ancho: {self._ancho}, Alto: {self._alto}]'
-    
+
+    def _validar_valores(self,valor): #Metodo encapsulado
+        return True if 0 < valor < 10 else False
