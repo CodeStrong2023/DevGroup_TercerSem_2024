f6d7f5e8de12a4b3e32a2a76a00d970761e8c58a
Angelo Vellar <angelovellar24@gmail.com>
2024-05-06 19:01:43 -0300

    Update: Agregando JS Videos 6-10 Clase 3

--- Javascript/Leccion03/01-03-funciones.js
+++ Javascript/Leccion03/01-03-funciones.js
@@ -32,3 +32,49 @@ miFuncionDos(5, 7, 3, 6);
 var miFuncionTexto = miFuncionDos.toString(); //convierte la funcion a texto
 console.log(miFuncionTexto);
 
+//Funciones flecha
+const sumarFuncionFlecha = (a,b)  => a + b;
+resultado = sumarFuncionFlecha(3,7); //Asignamos el valor a una variable
+console.log(resultado); 
+
+let sumar = function(a,b){
+    console.log(arguments[0]);     //muestra el parametro de : a
+    console.log(arguments[1]); //muestra el parametro de : b
+    console.log(arguments[2]);
+    return a+b + arguments[2];
+}
+resultado = sumar(3,2,9);
+console.log(resultado);
+
+//Sumar todos los argumentos
+let respuesta = sumarTodo(5,4,13,10,9);
+console.log(respuesta);
+function sumarTodo(){
+    let suma = 0;
+    for(let i = 0; i < arguments.length; i++){
+        suma += arguments[i] // arguments es para arreglos
+    }
+    return suma;
+}
+
+//Tipos primitivos
+let k = 10;
+function cambiarValor(a){ // paso por valor
+    a = 20;
+}
+cambiarValor(k);
+console.log(k);
+
+// Paso por referencia
+const persona = {
+    nombre: 'Juan',
+    apellido: 'Lopez'
+}
+console.log(persona);
+function cambiarValorObjeto(p1){
+    p1.nombre = 'Ignacio';
+    p1.apellido = 'Perez';
+}
+
+cambiarValorObjeto(persona);
+console.log(persona);
