202487b7b63cbfd140f16ebff7f3fc75d12ba56f
Marcos Martos <marcosmartostuvi@gmail.com>
2024-05-07 20:14:27 -0300

    python clase3 parte 9 polimorfismo: videos 1-3

---
+++ Python/Lección13/Empleado.py
@@ -0,0 +1,7 @@
+class Empleado: # No hereda sino solo de la clase object
+    def __init__(self, nombre, sueldo):
+        self.nombre = nombre
+        self.sueldo = sueldo 
+
+    def __str__(self):
+        return f'Empleado: [Nombre: {self.nombre}, Suledo: {self.sueldo}]'
