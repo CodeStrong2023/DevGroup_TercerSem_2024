a217d9690c608a69a171def0191061bad3560a12
¨alejandro <necro6700@gmail.com>
2024-05-15 09:39:04 -0300

    trabajo semana 4 de js video 3 y 4

--- Javascript/Lección04/01-04-objetos.js
+++ Javascript/Lección04/01-04-objetos.js
@@ -1,15 +1,26 @@
-let x = 10; //Variable de tipo primitiva
-console.log(x.lenght);
+let x = 10; 
+console.log(x.length); 
 
 //Objeto
 let persona = {
     nombre: 'Carlos',
     apellido: 'Gil',
     email: 'cgil@gmail.com',
-    edad: 30
+    edad: 30, 
+    nombreCompleto: function(){ // metodo o funcion en JavaScript
+        return this.nombre + ' ' + this.apellido;
+    }  
 }
 
-console.log(persona.nombre)
-console.log(persona.apellido)
-console.log(persona.email)
-console.log(persona.edad)
+console.log(persona.nombre); // "Carlos"
+console.log(persona.apellido); // "Gil"
+console.log(persona.email); // "cgil@gmail.com"
+console.log(persona.edad); // 30
+console.log(persona); // { nombre: 'Carlos', apellido: 'Gil', email: 'cgil@gmail.com', edad: 30}
+console.log(persona.nombreCompleto()); // "Carlos Gil"
+
+let persona2 = new Object(); //debe crear un nuevo objeto en memoria 
+persona2.nombre = 'Juan';
+persona2.direccion = 'salada 14';
+persona2.telefono = '5492618282821';
+console.log(persona2.telefono); 5492618282821
