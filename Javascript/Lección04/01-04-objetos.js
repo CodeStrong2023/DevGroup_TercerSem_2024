cdae06f0ef5cd5aacf9d0863da6b780d5592c5d5
BrunoOlivera8 <brunoolivera99.bo@gmail.com>
2024-05-17 18:34:46 -0300

    agregando ejercicios de JavaScript, semana 4 videos 7 y 8

--- Javascript/Lección04/01-04-objetos.js
+++ Javascript/Lección04/01-04-objetos.js
@@ -1,6 +1,6 @@
 let x = 10; 
 console.log(x.length); 
-
+console.log('Tipos primitivos'); 
 //Objeto
 let persona = {
     nombre: 'Carlos',
@@ -11,32 +11,47 @@ let persona = {
         return this.nombre + ' ' + this.apellido;
     }  
 }
-
 console.log(persona.nombre); // "Carlos"
 console.log(persona.apellido); // "Gil"
 console.log(persona.email); // "cgil@gmail.com"
 console.log(persona.edad); // 30
 console.log(persona); // { nombre: 'Carlos', apellido: 'Gil', email: 'cgil@gmail.com', edad: 30}
 console.log(persona.nombreCompleto()); // "Carlos Gil"
-
+console.log('Ejecutando con un objeto'); 
 let persona2 = new Object(); //debe crear un nuevo objeto en memoria 
 persona2.nombre = 'Juan';
 persona2.direccion = 'salada 14';
 persona2.telefono = '5492618282821';
 console.log(persona2.telefono); 5492618282821
-console.log('Creamos un nuevo objeto');
+console.log('Creamos un nuevo objeto'); 
 console.log(persona["apellido"]) //accedemos como si fuera un arreglo
-
+console.log('Usamos el ciclo for in'); 
 console.log('Usamos ciclo for in')
 //for in y accedemos al objeto como si fuera un arreglo
 for(propiedad in persona){
     console.log(propiedad);
     console.log(persona[propiedad]);
 }
-
-console.log('cambiamos y eliminamos el error')
+console.log('Cambiamos y eliminamos un error'); 
 persona.apellida = "Betancud"; //cambiamos dinamicamente un valor de un objeto
 delete persona.apellida; //Eliminamos el error
 console.log(persona);
 
+//Distintas formas de imprimir un objeto:
+//Numero 1: la mas sencilla: concatenar cada valor de cada propiedad
+console.log('Distintas formas de imprimir un objeto: forma 1');
+console.log(persona.nombre+', '+persona.apellido);
+//Numero 2: a traves del ciclo for in
+console.log('Distintas formas de imprimir un objeto: forma 2');
+for(nombrePropiedad in persona){
+    console.log(persona[nombrePropiedad]);
+}
+//Numero 3: la funcion Object.values()
+console.log('Distintas formas de imprimir un objeto: forma 3');
+let personaArray = Object.values(persona);
+console.log(personaArray);
 
+//Numero 4: utilizaremos el metodo JSON.stringify
+console.log('Distintas formas de imprimir un objeto: forma 4');
+let personaString = JSON.stringify(persona);
+console.log(personaString);
