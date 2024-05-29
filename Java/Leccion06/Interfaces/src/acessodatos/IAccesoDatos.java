f67e7eb5360fdfbbe6926ae0c2b0161988e7e73b
Gabriel <gabiggonzalez719@gmail.com>
2024-05-29 15:44:59 -0300

    Subo Manejo de Interfaces videos 1,2,3

---
+++ Java/Leccion06/Interfaces/src/acessodatos/IAccesoDatos.java
@@ -0,0 +1,17 @@
+
+package acessodatos;
+
+public interface IAccesoDatos {
+    int MAX_REGISTRO = 10;
+    
+    //Método insertar es abstracto y sin cuerpo
+    
+    void insertar();
+    
+    void listar();
+    
+    void actualizar();
+    
+    void eliminar();
+    
+}
