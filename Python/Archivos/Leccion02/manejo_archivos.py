83a8f497d8bf19c5fda59dadf7e04073cbef5cc3
Angelo Vellar <angelovellar24@gmail.com>
2024-05-30 11:46:55 -0300

    Update: Agregando carpeta archivos y leccion02 en Python

---
+++ Python/Archivos/Leccion02/manejo_archivos.py
@@ -0,0 +1,14 @@
+# Declaramos una variable
+try:
+    archivo = open('prueba.txt', 'w', encoding='utf8')  #La w es de write que significa escribir
+    archivo.write('Programamos con diferentes tipos de archivos,ahora en txt.\n')
+    archivo.write('Los acentos son importantes para las palabras\n')
+    archivo.write('Como por ejemplo: accion, ejecución y producción\n')
+    archivo.write('Con esto terminamos')
+
+except Exception as e:
+    print(e)
+finally: #Siempre se ejecuta
+    archivo.close() # Con esto se debe cerrar el archivo
+#archivo.write('Todo quedó perfecto'): este es un error
+
