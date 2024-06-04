4f5fa6d66c5765784efbe5371b736934dcb0c644
Joaquin Esposito <joaquiesposito@gmail.com>
2024-06-04 19:27:18 -0300

    subiendo leccion 06 Python y carpeta Leccion05 Java

--- Java/Leccion05/ClasesAbstractas/src/domain/FiguraGeometrica.java
+++ Java/Leccion05/ClasesAbstractas/src/domain/FiguraGeometrica.java
@@ -8,7 +8,6 @@ public abstract class FiguraGeometrica {
         this.tipoFigura = tipoFigura;
     }
     
-<<<<<<< HEAD
     //Metodo Abstracto
     public abstract void dibujar();
     
@@ -19,31 +18,13 @@ public abstract class FiguraGeometrica {
     }
 
     public void setTipoFigura(String tipoFigura) {
-=======
-    //Metodo abstracto
-    public abstract void dibujar();
-        
-    //Agregamos el get y set
-    
-    public String getTipoFigura(){
-        return tipoFigura;
-    }
-    public void setTipoFigura(){
->>>>>>> 7ed7ba0 (agregando ejercicios de Java, clase 5 videos 7 y 8)
         this.tipoFigura = tipoFigura;
     }
 
     @Override
     public String toString() {
         return "FiguraGeometrica{" + "tipoFigura=" + tipoFigura + '}';
-<<<<<<< HEAD
     } 
     
 }
 
-=======
-    }
-    
-    
-}
->>>>>>> 7ed7ba0 (agregando ejercicios de Java, clase 5 videos 7 y 8)
