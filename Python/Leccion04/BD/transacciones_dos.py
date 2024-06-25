769299ed284b1018d9feb07a97dfb0a182b917c4
BrunoOlivera8 <brunoolivera99.bo@gmail.com>
2024-06-25 20:07:02 -0300

    Agrego ejercicios de la clase 9 b de Python (videos del 4 al 7)

--- Python/Leccion04/BD/transacciones_dos.py
+++ Python/Leccion04/BD/transacciones_dos.py
@@ -2,12 +2,17 @@ import psycopg2 as bd #Esto es para poder conectarnos a Postgre
 
 conexion = bd.connect(user='postgres', password='admin', host='127.0.0.1', port='5432', database='test_bd')
 try:
-   # conexion.autocommit = False  # esto directamente no deberia estar
+    conexion.autocommit = False  # esto directamente no deberia estar, inicia la transaccion
     cursor = conexion.cursor()
     sentencia = 'INSERT INTO persona(nombre, apellido, email)VALUES(%s, %s, %s)'
-    valores = ('Maria', 'Esparza', 'mesparza@mail.com')
+    valores = ('Jorge', 'Prol', 'jprol@mail.com')
     cursor.execute(sentencia,valores)
-    conexion.commit() #hacemos el commit manualmente
+
+    sentencia = 'UPDATE persona SET nombre=%s, apellido=%s, email=%s WHERE id_persona=%s'
+    valores = ('Juan Carlo', 'Perez', 'jcperez@mail.com', 1)
+    cursor,execute(sentencia, valores)
+
+    conexion.commit() #hacemos el commit manualmente, se cierra la transaccion
     print('Termina la transaccion')
 except Exception as e:
     conexion.rollback()
