0aa02466e685f7784c2622226f1fbd6fda4c8151
Gabriel <gabiggonzalez719@gmail.com>
2024-06-29 02:37:51 -0300

    Clase 10 Java videos 1-6

---
+++ Java/Leccion10/ListadoPersonasApp.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class ListadoPersonasApp {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        //Definimos la lista fuera del ciclo while
+        List<Persona> personas = new ArrayList<>();
+        //Empezamos con el menu
+        var salir = false;
+        while (!salir){
+            mostrarMenu();
+
+
+
+        }//fin del ciclo while
+    }//Fin metodo Main
+
+
+}//Fin de la clase ListadoPersonasApp
